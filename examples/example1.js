7db074126d5ea1f9063792e6b288c280e46843c3
sourabh <ssourabh6436@gmail.com>
2026-01-21 13:37:45 +0530

    changes

---
+++ examples/example1.js
@@ -0,0 +1,6 @@
+// console is an object which give ton print the output
+//device
+/**
+ * this is an example of the multiline comment
+ */
+console.log('Hello World');
---
+++ examples/example2.js
@@ -0,0 +1,10 @@
+let x = 10;
+console.log(typeof x);
+console.log(x);
+
+x = 20;
+console.log(x);
+
+const y = 10;
+console.log(y);
+y = 20;
