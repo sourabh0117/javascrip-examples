9bbc58b3a2d67e5d7c54c21c837425c1da187e0d
sourabh <ssourabh6436@gmail.com>
2026-01-22 15:44:08 +0530

    flatmap and map function

---
+++ examples/example19.js
@@ -0,0 +1,33 @@
+const person =  [
+    [
+        {
+            name: "Tommy",
+            age: 2,
+        },
+        {
+            name: "pluto",
+            age: 3,
+        }
+    ],
+    [
+        {
+            name: "german",
+            age: 2,
+        },
+        {
+            name: "sundae",
+            age: 5,
+        }  
+    ],
+    
+];
+
+const names = person.map((arr, i) =>{
+    return arr.map(p => p.name);
+});
+console.log(names);
+
+
+// person.flatMap((arr, i) => {
+//     return  arr.map(p => p.name);
+// }).forEach(n => console.log(n));
