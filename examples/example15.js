a5336f2881683e7349b98de96decc032d4b443a3
sourabh <ssourabh6436@gmail.com>
2026-01-22 10:49:09 +0530

    Arrow Function

---
+++ examples/example15.js
@@ -0,0 +1,14 @@
+// const add = function addNumber(a, b){
+//     return a+ b;
+// }
+// const r = add(10, 20);
+// console.log(r);
+
+
+
+// arrow funtion is simplest way write the function in easy manner no need to write funtion name and return
+const add = (a, b) =>  a + b;// if argument is single no need to write the paranthesis
+
+
+const r = add(10, 20);
+console.log(r);
